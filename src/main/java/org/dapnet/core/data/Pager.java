5e496f0d36917025dda9c9b6873bdda91d15da69
Philipp Thiel <Taronyu@users.noreply.github.com>
2018-01-14 15:21:08 +0100

    Added pager object

---
+++ src/main/java/org/dapnet/core/data/Pager.java
@@ -0,0 +1,24 @@
+package org.dapnet.core.data;
+
+public class Pager {
+
+	private int number;
+	private String name;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
