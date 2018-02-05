1f931a6cc492a6edda1e6ee15c8a828623051337
Philipp Thiel <Taronyu@users.noreply.github.com>
2018-02-05 01:28:51 +0100

    Hibernate JPA stuff

--- src/main/java/org/dapnet/core/data/User.java
+++ src/main/java/org/dapnet/core/data/User.java
@@ -1,34 +1,46 @@
 package org.dapnet.core.data;
 
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * This class represents a user account.
+ * 
+ * @author Philipp Thiel
+ */
+@Entity(name = "User")
+@Table(name = "users")
 public class User implements NamedObject {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(unique = true, nullable = false)
 	private String name;
-	private String hash;
-	private String mail;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false)
+	private String email;
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Instant createdOn;
+	@Column(nullable = false)
 	private boolean admin;
 
-	public String getHash() {
-		return hash;
+	public int getId() {
+		return id;
 	}
 
-	public void setHash(String hash) {
-		this.hash = hash;
-	}
-
-	public String getMail() {
-		return mail;
-	}
-
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-
-	public boolean isAdmin() {
-		return admin;
-	}
-
-	public void setAdmin(boolean admin) {
-		this.admin = admin;
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -37,6 +49,38 @@ public class User implements NamedObject {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Instant getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Instant createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
 	}
 
 	@Override
